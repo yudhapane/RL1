2c9426538f20fcf648f5c885cf6ed2088c17d1ca
unknown <yudhapane@yahoo.com>
2014-12-10 11:35:16 +0100

    Add rl-book folder containing codes of sutton book

--- rl-course/matlab/eps_greedy.m
+++ rl-course/matlab/eps_greedy.m
@@ -4,18 +4,20 @@ close all; clear;
 plays = 1000;
 tasks = 100;
 
-eps = [0.1 0.01 0];
+eps = [0 0.01 0.1];
 colors =[0, 0, 1;
          1, 0, 0;
          0, 0, 0];
 figure;
 for i = 1:length(eps)
-    [r,o] = testbandit(eps(i), tasks, plays);
+    [r,o,Q,k] = testbandit(eps(i), tasks, plays);
     subplot(2,1,1);
     plot(r, 'Color', colors(i,:));  hold on;
     subplot(2,1,2);
     plot(o, 'Color', colors(i,:));  hold on;
 end
+Q
+k
 subplot(2,1,1);axis([0 plays -0.3 2]);
 legend(['eps=', num2str(eps(1))],['eps=', num2str(eps(2))],['eps=', ...
     num2str(eps(3))]);
--- rl-course/matlab/testbandit.m
+++ rl-course/matlab/testbandit.m
@@ -1,4 +1,4 @@
-function [r, o] = testbandit(eps, tasks, plays)
+function [r, o, Q, k] = testbandit(eps, tasks, plays)
 %TESTBANDIT Test evaluative feedback performance on n-armed bandit problem
 %   R = testbandit(N, EPS) returns the average performance
 %   of evaluative feedback learning on the N-armed bandit with exploration
@@ -18,50 +18,63 @@ function [r, o] = testbandit(eps, tasks, plays)
     % epsilon greedy probability array initialization    
     pa = eps/(N); % sampling probability of each action
     
+    % Initialize Q table
+    Q = zeros(plays, N);
+    QS = zeros(1,N); % sum variable
+    
     for ii=1:tasks
         % Get a random bandit and its associated optimal action
         [bandit, opt] = getbandit(N);
         astar = opt;
         % Initialize action counter
         k = zeros(1, N);
-
-        % Initialize Q table
-        Q = zeros(1, N);
-        
+      
         R = zeros(1,plays);
-        for pp=2:plays
+     
+        for pp=1:plays            
             
-            % Epsilon-greedy action selection           
+            % get (current) maximum action
+            if pp == 1
+                astar = opt;
+            else                
+                [maxR, astar] = max(Q(pp-1,:));
+            end
+            
+            % Epsilon-greedy action selection
             p = pa*ones(N,1); % reset probability
             p(astar) = p(astar)+ 1-eps;
             
+            % sample action
             a = sample(p);
+            k(a) = k(a) + 1;
             
             % Run the bandit
             r = bandit(a);
             
-            % Incremental sample averaging
-            R(pp) = R(pp-1) + 1/(pp)*(r-R(pp-1));
+              
             
-            % Update action counter
-            k(a) = k(a) + 1;
-            
-            % TODO: Update Q table
+            % Update Q table
             if(k(a) == 0)
-                Q(a) = 0;
-            else                 
-                Q(a) = (r + Q(a))/k(a);
+                Q(pp,a) = 0;
+            else     
+                QS(a) = QS(a)+r;
+                Q(pp,a) = (QS(a))/k(a);
+            end            
+            
+            % Incremental sample averaging
+            if pp == 1
+                R(pp) = 0;
+            else
+                R(pp) = R(pp-1) + 1/(pp)*(r-R(pp-1));
             end
-                
-            [maxR, astar] = max(Q);
+            
+            % Update action counter             
             
             % Update record keeping
 %             rewards(pp) = rewards(pp) + r;
             if a == opt
                 optactions(pp) = optactions(pp) + 1;
-            end
-            
-
+            end           
         end
         rewards = rewards+R;
     end
