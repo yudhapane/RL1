a086384309a6cb6afe03ad34bb237a9641153f55
unknown <yudhapane@yahoo.com>
2014-12-10 14:41:58 +0100

    N-arm bandit program fixed

--- rl-course/matlab/eps_greedy.m
+++ rl-course/matlab/eps_greedy.m
@@ -2,26 +2,25 @@
 %the cumulative reward
 close all; clear;
 plays = 1000;
-tasks = 100;
+tasks = 300;
 
-eps = [0 0.01 0.1];
+tEps = [0.1 0 0.2];
 colors =[0, 0, 1;
          1, 0, 0;
          0, 0, 0];
 figure;
-for i = 1:length(eps)
-    [r,o,Q,k] = testbandit(eps(i), tasks, plays);
+for i = 1:length(tEps)
+    [r,o,Q,k] = testbandit(tEps(i), tasks, plays);
     subplot(2,1,1);
     plot(r, 'Color', colors(i,:));  hold on;
     subplot(2,1,2);
     plot(o, 'Color', colors(i,:));  hold on;
 end
-Q
-k
+
 subplot(2,1,1);axis([0 plays -0.3 2]);
-legend(['eps=', num2str(eps(1))],['eps=', num2str(eps(2))],['eps=', ...
-    num2str(eps(3))]);
+legend(['tEps=', num2str(tEps(1))],['tEps=', num2str(tEps(2))],['tEps=', ...
+    num2str(tEps(3))]);
 
 subplot(2,1,2);axis([0 plays 0 1.2]);
-legend(['eps=', num2str(eps(1))],['eps=', num2str(eps(2))],['eps=', ...
-    num2str(eps(3))]);
+legend(['tEps=', num2str(tEps(1))],['tEps=', num2str(tEps(2))],['tEps=', ...
+    num2str(tEps(3))]);
